b87540c0ebbca7a1328d23e3c98a613457ad55c0
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-03-19 18:30:22 -0300

    Redux stores for username and cart
    Hooked up the cart to the menu so pizzas can be added when clicking on the menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -10,12 +10,19 @@ const styles = {
     "inline-block text-sm rounded-full border-2 border-stone-300 font-normal uppercase tracking-wide text-stone-300 transition-colors duration-300 hover:bg-stone-300  hover:text-stone-800 hover:text-stone-800 focus:outline-none focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
 };
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   if (to)
     return (
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
+    );
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
     );
 
   return (
